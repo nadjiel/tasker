b4902758201436d7791b3eeb9700b74a6d1771b7
Daniel O Sousa <oliveira.daaaaniel@gmail.com>
2023-10-10 20:50:40 -0300

    Initial commit

---
+++ src/test/java/com/nadjiel/tasker/TaskerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nadjiel.tasker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
