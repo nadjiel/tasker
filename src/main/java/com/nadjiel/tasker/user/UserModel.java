2c1ad8f5f9c309111ac6d48fb8270820b3537e96
Daniel O Sousa <oliveira.daaaaniel@gmail.com>
2023-10-10 21:07:51 -0300

    refactor: adding lombok to the project

--- src/main/java/com/nadjiel/tasker/user/UserModel.java
+++ src/main/java/com/nadjiel/tasker/user/UserModel.java
@@ -1,33 +1,12 @@
 package com.nadjiel.tasker.user;
 
+import lombok.Data;
+
+@Data
 public class UserModel {
   
   private String username;
   private String password;
   private String name;
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public String getUsername() {
-    return this.username;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  public String getPassword() {
-    return this.password;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getName() {
-    return this.name;
-  }
 
 }
