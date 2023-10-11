a780e9b7fc1f7e2841546a922264c808cb2a5301
Daniel O Sousa <oliveira.daaaaniel@gmail.com>
2023-10-10 21:26:05 -0300

    feat: making the UserModel be an Entity that creates a table on the database

--- src/main/java/com/nadjiel/tasker/user/UserModel.java
+++ src/main/java/com/nadjiel/tasker/user/UserModel.java
@@ -1,12 +1,27 @@
 package com.nadjiel.tasker.user;
 
-import lombok.Data;
+import java.util.UUID;
+import java.time.LocalDateTime;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity(name = "t_users")
 @Data
 public class UserModel {
+
+  @Id
+  @GeneratedValue(generator = "UUID")
+  private UUID id;
   
   private String username;
   private String password;
   private String name;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
 
 }
