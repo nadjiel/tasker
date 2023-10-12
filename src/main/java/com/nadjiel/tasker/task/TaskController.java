b4c8137e1c5bb056b0788432002c1f1dd6dbf613
Daniel O Sousa <oliveira.daaaaniel@gmail.com>
2023-10-12 10:34:27 -0300

    feat: adding verification to the date attributes of a task on creation

--- src/main/java/com/nadjiel/tasker/task/TaskController.java
+++ src/main/java/com/nadjiel/tasker/task/TaskController.java
@@ -1,13 +1,17 @@
 package com.nadjiel.tasker.task;
 
 import java.util.UUID;
+import java.time.LocalDateTime;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.beans.factory.annotation.Autowired;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+
 
 @RestController
 @RequestMapping("/tasks")
@@ -17,7 +21,7 @@ public class TaskController {
   private ITaskRepository repository;
 
   @PostMapping("/")
-  public TaskModel create(
+  public ResponseEntity create(
     @RequestBody TaskModel model,
     HttpServletRequest request
   ) {
@@ -27,11 +31,28 @@ public class TaskController {
     // Setting the owner id in the model
     model.setOwner(owner);
 
+    // Verifying dates
+    LocalDateTime currentDate = LocalDateTime.now();
+
+    if(model.getStartsAt().isBefore(currentDate)) {
+      return ResponseEntity
+        .status(HttpStatus.BAD_REQUEST)
+        .body("startsAt can't be a past date!");
+    }
+
+    if(model.getFinishesAt().isBefore(model.getStartsAt())) {
+      return ResponseEntity
+        .status(HttpStatus.BAD_REQUEST)
+        .body("finishesAt can't be prior to the startsAt!");
+    }
+
     // Saving the task on the database
     TaskModel task = repository.save(model);
 
     // Returning result
-    return task;
+    return ResponseEntity
+      .status(HttpStatus.CREATED)
+      .body(task);
   }
 
 }
